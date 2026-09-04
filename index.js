3653b37189784f0ec7a62986e0062ccb280b51fb
Mary Lou Freil <maryloufreil@gmail.com>
2026-09-04 17:48:33 -0400

    fixed syntax error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const sqlite3 = require('sqlite3').verbose();
 // Initialize the express app
 const app = express();
 app.use(cors({
-  origin: 'httpsmyalbums.maryloufreil.com', // Specify your React app's URL exactly (no trailing slash)
+  origin: 'https:myalbums.maryloufreil.com', // Specify your React app's URL exactly (no trailing slash)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
