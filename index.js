b1dabcb248e59017925ca9cac5e42278fc7a5879
Mary Lou Freil <maryloufreil@gmail.com>
2026-09-05 12:46:44 -0400

    clearing cloudflare cache and updating CORS settings to allow requests from the deployed React app.

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const app = express();
 app.use(cors({
   origin: 'https:myalbums.maryloufreil.com', // Specify your React app's URL exactly (no trailing slash)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  cacheControl: ['no-cache', 'no-store', 'must-revalidate', 'proxy-revalidate' ],
+  pragma: ['no-cache'],
+    expires: ['0'],
 }));
 
 
